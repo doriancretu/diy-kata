6cbad3bae40b22e7678c470599ec67d92bee31b9
joestephens <joe.stephens@live.co.uk>
2018-02-13 23:11:48 +0000

    FizzBuzz boilerplate

---
+++ __tests__/kata1.fizzBuzz.test.js
@@ -0,0 +1,15 @@
+import { fizzBuzz } from '../src'
+
+describe('fizzBuzz', () => {
+  it('returns fizz when passed a multiple of 3', () => {
+
+  })
+
+  it('returns buzz when passed a multiple of 5', () => {
+
+  })
+
+  it('returns fizzbuzz when passed a multiple 3 and 5', () => {
+
+  })
+})
