f03df60fe13a7b7d1e95fbec30c219ef758c1629
doriancretu <dorianalin.cretu@yahoo.com>
2019-10-17 20:40:57 +0100

    completed kata1

--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -1,3 +1,14 @@
-const fizzBuzz = number => {};
+const fizzBuzz = number => {
+    if (number % 3 === 0) {
+        return "Fizz";
+    } else if (number % 5 === 0) {
+    return "Buzz;"
+    } else if(number % 3 === 0 && number % 5 === 0) {
+        return FizzBuzz;
+    } else {
+        return number;
+    }
+
+};
 
 module.exports = fizzBuzz;
